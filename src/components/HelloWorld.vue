be73dba66ecc73ec9b9e79ad81161e6f8c014e5e
Stella Maia <stellamaia97@hotmail.com>
2024-03-31 21:41:08 -0300

    Implementação HelloWorld

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,60 +1,142 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-router" target="_blank" rel="noopener">router</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-vuex" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div class="payment">
+    <h1>Checkout</h1>
+    <div style="justify-content: center; display: grid;">
+    <div v-if="step === 1">
+      <h2>Escolha a opção de pagamento:</h2>
+      <div class="payment-option-container">
+        <div v-for="option in paymentOptions" :key="option.method" class="payment-option">
+          <img :src="option.icon" :alt="option.alt">
+          <button class="payment-buttons" @click="choosePayment(option.method)">{{ option.label }}</button>
+        </div>
+      </div>
+    </div>
+    <div v-else-if="step === 2">
+      <h2 class="title-information">Preencher informações adicionais:</h2>
+      <component :is="paymentMethodFormComponent"></component>
+      <button class="confirm-credit-payment" @click="confirmPayment">Confirmar Pagamento</button>
+    </div>
+    </div>
+    <!-- Adicione outras etapas aqui -->
   </div>
 </template>
 
 <script>
+import CreditCardForm from './CreditCardForm.vue'; // Importar componente para formulário de cartão de crédito
+import BoletoForm from './BoletoForm.vue'; // Importar componente para formulário de boleto
+import PixForm from './PixForm.vue'; // Importar componente para formulário de Pix
+
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
+  data() {
+    return {
+      step: 1,
+      paymentMethod: null,
+      creditCard: {
+        number: '',
+        cvv: '',
+        expiryDate: '',
+        cardHolderName: '',
+        cpf: '',
+        installments: 1,
+      },
+      boleto: {
+        cpf: '',
+        name: '',
+        lastName: '',
+        email: '',
+        barcode: '',
+        dueDate: '',
+      },
+      pix: {
+        qrCode: '',
+        expirationTime: '',
+      },
+      products: [],
+      purchaseDate: '',
+      deliveryDate: '',
+      paymentOptions: [
+        {
+          method: 'cartao',
+          label: 'Cartão de Crédito',
+          icon: require('@/assets/credit-card.png'),
+          alt: 'Ícone de cartão de crédito',
+          formComponent: 'CreditCardForm',
+          styleClass: 'pix-icon', // Adicionando uma classe CSS para o ícone do Pix
+
+        },
+        {
+          method: 'boleto',
+          label: 'Boleto',
+          icon: require('@/assets/boleto.jpg'),
+          alt: 'Ícone de boleto',
+          formComponent: 'BoletoForm',
+          styleClass: 'pix-icon', // Adicionando uma classe CSS para o ícone do Pix
+
+        },
+        {
+          method: 'pix',
+          label: 'Pix',
+          icon: require('@/assets/pix-icon.png'),
+          alt: 'Ícone de Pix',
+          formComponent: 'PixForm',
+          styleClass: 'pix-icon', // Adicionando uma classe CSS para o ícone do Pix
+
+        },
+      ],
+    };
+  },
+  computed: {
+    paymentMethodFormComponent() {
+      if (this.paymentMethod) {
+        return this.paymentOptions.find(option => option.method === this.paymentMethod).formComponent;
+      }
+      return null;
+    }
+  },
+  methods: {
+    choosePayment(method) {
+      this.paymentMethod = method;
+      this.step = 2;
+    },
+    confirmPayment() {
+      // Lógica para confirmar o pagamento
+      this.step = 3;
+    },
+  },
+  components: {
+    CreditCardForm,
+    BoletoForm,
+    PixForm,
+  },
+};
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped lang="scss">
-h3 {
-  margin: 40px 0 0;
+<style scoped>
+
+#app > div > div > div > div > div > div:nth-child(1) > img,
+#app > div > div > div > div > div > div:nth-child(2) > img,
+#app > div > div > div > div > div > div:nth-child(3) > img
+{
+  height: 30px!important;
+  width: 30px!important;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+.payment-option{
+  display: flex;
+  padding-bottom: 10px;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+.payment-buttons{
+  margin-left: 10px;
+  cursor: pointer;
+  
 }
-a {
-  color: #42b983;
+.confirm-credit-payment{ 
+  height: 40px;
+  cursor: pointer;
+  border: 1px solid rgb(0, 0, 0);
+  padding: 10px;
+  border-radius: 5px;
+}
+.title-information{
+  padding-bottom: 20px;
 }
 </style>
