84c2b15c00cac6a9d98e43b5afd75f4f4a445ac7
Stella Maia <stellamaia97@hotmail.com>
2024-04-01 23:09:21 -0300

    Implementação de rules

--- src/components/CreditCardForm.vue
+++ src/components/CreditCardForm.vue
@@ -1,77 +1,34 @@
 <template>
-  <div style="text-align: start;">
-    <div class="information-container">
+  <div class="credit-card-container">
+    <v-form ref="form" v-model="valid" lazy-validation>
+      <v-select v-model="flags" :items="listFlags" label="Bandeira" :rules="flagsRules"></v-select>
+
+      <v-text-field v-model="number" :rules="numberRules" label="Número do Cartão" required></v-text-field>
+
       <v-row>
         <v-col cols="6">
-          <label class="label">Número do Cartão:</label>
+          <v-text-field v-model="cvv" :counter="3" label="CVV" :rules="cvvRules" required></v-text-field>
         </v-col>
         <v-col cols="6">
-          <input class="input" type="text" v-model="formData.number">
+          <v-text-field v-model="expiryDate" label="Vencimento" :rules="expiryDateRules" required
+            hint="Formato: MM/YYYY"></v-text-field>
         </v-col>
       </v-row>
-    </div>
 
-    <div class="information-container">
+      <v-text-field v-model="name" label="Nome" :rules="nameRules" required></v-text-field>
+
+
       <v-row>
         <v-col cols="6">
-          <label class="label">CVV:</label>
+          <v-text-field v-model="cpf" :counter="11" label="CPF" :rules="cpfRules" required></v-text-field>
         </v-col>
         <v-col cols="6">
-          <input class="input" type="text" v-model="formData.cvv">
+          <v-text-field v-model="cnpj" :counter="14" label="CNPJ" :rules="cnpjRules" required></v-text-field>
         </v-col>
       </v-row>
-    </div>
+      <v-btn class="confirm-credit-payment" @click="confirmPayment"  >Confirmar Pagamento</v-btn>
 
-    <div class="information-container">
-      <v-row>
-        <v-col cols="6">
-          <label class="label">Data de Vencimento:</label>
-        </v-col>
-        <v-col cols="6">
-          <input class="input" type="text" v-model="formData.expiryDate">
-        </v-col>
-      </v-row>
-    </div>
-
-    <div class="information-container">
-      <v-row>
-        <v-col cols="6">
-          <label class="label">Nome do Titular:</label>
-        </v-col>
-        <v-col cols="6">
-          <input class="input" type="text" v-model="formData.cardHolderName">
-        </v-col>
-      </v-row>
-    </div>
-
-
-    <div class="information-container">
-      <v-row>
-
-        <v-col cols="6">
-          <label class="label">CPF/CNPJ:</label>
-        </v-col>
-
-        <v-col cols="6">
-          <input class="input" type="text" v-model="formData.cpf">
-        </v-col>
-      </v-row>
-    </div>
-
-
-    <div class="information-container">
-      <v-row>
-
-        <v-col cols="6">
-          <label class="label">Número de Parcelas:</label>
-        </v-col>
-
-        <v-col cols="6">
-          <input class="input" type="number" v-model="formData.installments">
-        </v-col>
-      </v-row>
-    </div>
-
+    </v-form>
   </div>
 </template>
 
@@ -79,19 +36,83 @@
 export default {
   data() {
     return {
-      formData: {
-        number: '',
-        cvv: '',
-        expiryDate: '',
-        cardHolderName: '',
-        cpf: '',
-        installments: 1,
-      }
+      listFlags: ['Mastercard', 'Visa'],
+      flags: '',
+      number: '',
+      cvv: '',
+      expiryDate: '',
+      name: '',
+      cpf: '',
+      cnpj: '',
+      valid: false, // Define se o formulário é válido ou não
+      showErrorMessage: false, // Variável para controlar a exibição da mensagem de erro
+
+      flagsRules: [
+        v => !!v || 'Bandeira é obrigatória',
+
+      ],
+      numberRules: [
+        value => !!value || 'Número do cartão é obrigatório',
+        value => {
+          const cleanedNumber = value.replace(/\s|-/g, '');
+          if (!cleanedNumber.match(/^\d{13,16}$/)) {
+            return ' número de cartão inválido. Deve ter entre 13 e 16 dígitos.';
+          }
+          return true;
+        }
+      ],
+      cvvRules: [
+        v => !!v || 'CVV é obrigatório',
+        v => v.length === 3 || 'CVV deve ter 3 dígitos'
+      ],
+      expiryDateRules: [
+        v => !!v || 'Data de validade é obrigatória',
+        v => /^(0[1-9]|1[0-2])\/20[2-9][0-9]$/.test(v) || 'Formato de data inválido (MM/YYYY)'
+
+      ],
+      nameRules: [
+        v => !!v || 'Nome é obrigatório', // Verifica se o campo não está vazio
+      ],
+      cpfRules: [
+        v => !!v || 'CPF é obrigatório', // Verifica se o campo não está vazio
+        v => v.length === 11 || 'CPF deve conter 11 digitos'
+
+      ],
+      cnpjRules: [
+        v => !!v || 'CNPJ é obrigatório', // Verifica se o campo não está vazio
+        v => v.length === 14 || 'CNPJ deve ter 14 dígitos'
+
+      ],
+
     };
+  },
+  methods: {
+    confirmPayment() {
+    // Verificar se o formulário é válido
+    if (this.$refs[`form`].validate()) {
+      
+      // Se o formulário for válido, avance para o passo 3
+      console.log('Formulário válido');
+      this.step = 3;
+
+    } 
+    else{
+      // Se o formulário não for válido, exiba mensagem
+      console.log('Preencha todo o Formulário ');
+
+    }
   }
+  }
+
+  
 };
 </script>
+
 <style scoped>
+.credit-card-container {
+  padding-bottom: 40px;
+}
+
 .label {
   font-weight: bolder;
   padding-right: 10px;
@@ -99,7 +120,6 @@ export default {
 }
 
 .information-container {
-
   padding-bottom: 10px;
 }
 
