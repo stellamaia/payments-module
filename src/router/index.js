5e2183d6b852effeaf9896491df18f62a68b9c4b
Stella Maia <stellamaia97@hotmail.com>
2024-04-02 23:09:34 -0300

    Remoção de view e rota de HomeView

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,15 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+// import HomeView from '../views/HomeView.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
-  {
-    path: '/',
-    name: 'home',
-    component: HomeView
-  },
+  // {
+  //   path: '/',
+  //   name: 'home',
+  //   component: HomeView
+  // },
 ]
 
 const router = new VueRouter({
